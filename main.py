8edc646cd19944974d63946b1590706705ee9407
stevelau1015 <1463089520@qq.com>
2022-04-19 10:22:08 +0800

    urllib.request 模块提供了最基本的构造 HTTP 请求的方法，利用它可以模拟浏览器的一个请求发起过程，同时它还带有处理授权验证（authentication）、重定向（redirection)、浏览器 Cookies 以及其他内容。 下面我们来看一下它的强大之处。这里以 Python 官网为例，我们来把这个网页抓下来：

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from urllib import request, parse
+import urllib.request  
 
-url = 'http://httpbin.org/post'
-headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
-    'Host': 'httpbin.org'
-}
-dict = {'name': 'Germey'}
-data = bytes(parse.urlencode(dict), encoding='utf8')
-req = request.Request(url=url, data=data, headers=headers, method='POST')
-response = request.urlopen(req)
-#print(response.read().decode('utf-8'))
+response = urllib.request.urlopen('https://www.python.org')  
+print(response.read().decode('utf-8'))
