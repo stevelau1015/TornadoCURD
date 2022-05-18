52633a98d4d09df2e9ec614ce54043c48ac725ad
stevelau1015 <1463089520@qq.com>
2022-05-18 22:57:36 +0800

    需要扫码登录微信公众号，使用个人平台的接口进行爬取，每天大约3000条，每小时300条

--- main.py
+++ main.py
@@ -1,11 +1,221 @@
-from urllib import request, parse
+# -*- coding: utf-8 -*-
+import logging
+import logging.handlers
+from selenium import webdriver
+import time
+import json
+import requests
+import re
+import random
+import os
+import sys
 
-url = 'http://httpbin.org/post'
-headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
-    'Host': 'httpbin.org'
-}
-dict = {'name': 'Germey'}
-data = bytes(parse.urlencode(dict), encoding='utf8')
-req = request.Request(url=url, data=data, headers=headers, method='POST')
-response = request.urlopen(req)
-#print(response.read().decode('utf-8'))
+from loggerr import Logger
+
+fh = logging.handlers.TimedRotatingFileHandler('./logs/http-info.log', when='midnight', backupCount=7, encoding='utf8')
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(message)s",
+    level=logging.DEBUG,
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[fh],
+)
+
+static_path = os.path.join(os.path.dirname(__file__), "static")
+
+# 微信公众号账号
+from selenium.webdriver.common.by import By
+
+user = "email"
+# 公众号密码
+password = "pwd"
+# 设置要爬取的公众号列表
+gzlist = ['白帽汇安全', '国际安全智库', '黑客技术与网络安全', '虎符智库', '互联网安全内参', '计算机与网络安全',
+          '零日情报局', '漏洞战争', '绿盟科技研究通讯', '弥天安全实验室', '木星安全实验室', '奇安网情局', '秦安战略', '清华大学战略与安全研究中心', '赛博研究院', '嘶吼专业版',
+          '腾讯安全威胁情报中心', '网安杂谈', '网电空间战', '网空安全观察', '网空闲话', '网络安全观', '网络空间安全军民融合创新中心', '小贝说安全', '行长叠报', '依蓝云', '邑安全',
+          '远望智库', '占知智库', '开源网安', 'OWASP', '深信服千里目安全实验室', '网电空间战']
+print(gzlist)
+
+
+# 登录微信公众号，获取登录之后的cookies信息，并保存到本地文本中
+def weChat_login():
+    # 定义一个空的字典，存放cookies内容
+    try:
+
+        post = {}
+
+        # 用webdriver启动谷歌浏览器
+        print("启动浏览器，打开微信公众号登录界面")
+        CHROME_DRIVER = r"./chromedriver"
+        driver = webdriver.Chrome(executable_path=CHROME_DRIVER)
+        # 打开微信公众号登录页面
+        driver.get('https://mp.weixin.qq.com/')
+        # 等待5秒钟
+        # time.sleep(5)
+        # print("正在输入微信公众号登录账号和密码......")
+        # 清空账号框中的内容
+        # driver.find_element(by=By.XPATH, value="./*//input[@id='account']").clear()
+        # 自动填入登录用户名
+        # driver.find_element(by=By.XPATH, value="./*//input[@id='account']").send_keys(user)
+        # 清空密码框中的内容
+        # driver.find_element(by=By.XPATH, value="./*//input[@id='pwd']").clear()
+        # 自动填入登录密码
+        # driver.find_element(by=By.XPATH, value="./*//input[@id='pwd']").send_keys(password)
+
+        # 在自动输完密码之后需要手动点一下记住我
+        # print("请在登录界面点击:记住账号")
+        # time.sleep(10)
+        # 自动点击登录按钮进行登录
+        # driver.find_element(by=By.XPATH, value="./*//a[@id='loginBt']").click()
+        # 拿手机扫二维码！
+        print("请拿手机扫码二维码登录公众号")
+        time.sleep(15)
+        print("登录成功")
+        # 重新载入公众号登录页，登录之后会显示公众号后台首页，从这个返回内容中获取cookies信息
+        driver.get('https://mp.weixin.qq.com/')
+        # 获取cookies
+        cookie_items = driver.get_cookies()
+        print(type(cookie_items))
+        print(cookie_items)
+        # 获取到的cookies是列表形式，将cookies转成json形式并存入本地名为cookie的文本中
+        for cookie_item in cookie_items:
+            post[cookie_item['name']] = cookie_item['value']
+        print(type(post))
+        print(post)
+
+        cookie_str = json.dumps(post)
+        print(type(cookie_str))
+        print(cookie_str)
+        with open('cookie.txt', 'w+', encoding='utf-8') as f:
+            f.write(cookie_str)
+        print("cookies信息已保存到本地")
+
+    except Exception as error:
+        logging.exception(error, exc_info=True)
+
+
+# 爬取微信公众号文章，并存在本地文本中
+def get_content(query):
+    # query为要爬取的公众号名称
+    # 公众号主页
+    url = 'https://mp.weixin.qq.com'
+    # 设置headers
+    header = {
+        "HOST": "mp.weixin.qq.com",
+        "User-Agent": "Mozilla/6.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0"
+    }
+
+    # 读取上一步获取到的cookies
+    with open('cookie.txt', 'r', encoding='utf-8') as f:
+        cookie = f.read()
+    cookies = json.loads(cookie)
+
+    # 登录之后的微信公众号首页url变化为：https://mp.weixin.qq.com/cgi-bin/home?t=home/index&lang=zh_CN&token=1849751598，从这里获取token信息
+    response = requests.get(url=url, cookies=cookies)
+    token = re.findall(r'token=(\d+)', str(response.url))[0]
+
+    # 搜索微信公众号的接口地址
+    search_url = 'https://mp.weixin.qq.com/cgi-bin/searchbiz?'
+    # 搜索微信公众号接口需要传入的参数，有三个变量：微信公众号token、随机数random、搜索的微信公众号名字
+    query_id = {
+        'action': 'search_biz',
+        'token': token,
+        'lang': 'zh_CN',
+        'f': 'json',
+        'ajax': '1',
+        'random': random.random(),
+        'query': query,
+        'begin': '0',
+        'count': '5'
+    }
+    # 打开搜索微信公众号接口地址，需要传入相关参数信息如：cookies、params、headers
+    search_response = requests.get(search_url, cookies=cookies, headers=header, params=query_id)
+    # 取搜索结果中的第一个公众号
+    lists = search_response.json().get('list')[0]
+    # 获取这个公众号的fakeid，后面爬取公众号文章需要此字段
+    fakeid = lists.get('fakeid')
+
+    # 微信公众号文章接口地址
+    appmsg_url = 'https://mp.weixin.qq.com/cgi-bin/appmsg?'
+    # 搜索文章需要传入几个参数：登录的公众号token、要爬取文章的公众号fakeid、随机数random
+    query_id_data = {
+        'token': token,
+        'lang': 'zh_CN',
+        'f': 'json',
+        'ajax': '1',
+        'random': random.random(),
+        'action': 'list_ex',
+        'begin': '0',  # 不同页，此参数变化，变化规则为每页加5
+        'count': '5',
+        'query': '',
+        'fakeid': fakeid,
+        'type': '9'
+    }
+    # 打开搜索的微信公众号文章列表页
+    appmsg_response = requests.get(appmsg_url, cookies=cookies, headers=header, params=query_id_data)
+    # 获取文章总数
+    max_num = appmsg_response.json().get('app_msg_cnt')
+    print("历史文章总数为", max_num)
+    print(max_num)
+    # 每页至少有5条，获取文章总的页数，爬取时需要分页爬
+    num = int(int(max_num) / 5)
+    # 起始页begin参数，往后每页加5
+    begin = 0
+    while num + 1 > 0:
+        query_id_data = {
+            'token': token,
+            'lang': 'zh_CN',
+            'f': 'json',
+            'ajax': '1',
+            'random': random.random(),
+            'action': 'list_ex',
+            'begin': '{}'.format(str(begin)),
+            'count': '5',
+            'query': '',
+            'fakeid': fakeid,
+            'type': '9'
+        }
+        print('正在翻页：--------------', begin)
+
+        # 获取每一页文章的标题和链接地址，并写入本地文本中
+        query_fakeid_response = requests.get(appmsg_url, cookies=cookies, headers=header, params=query_id_data)
+        fakeid_list = query_fakeid_response.json().get('app_msg_list')
+        for item in fakeid_list:
+            content_link = item.get('link')
+            content_title = item.get('title')
+            fileName = query + '.txt'
+            with open(fileName, 'a', encoding='utf-8') as fh:
+                fh.write(content_title + ":\n" + content_link + "\n")
+        num -= 1
+        begin = int(begin)
+        begin += 5
+        time.sleep(60)
+
+
+if __name__ == '__main__':
+    try:
+        logging.debug('debug 信息')
+        logging.info('info 信息')
+        logging.warning('warning 信息')
+        logging.error('error 信息')
+        logging.critical('critial 信息')
+        log_path = './Logs/'
+        if not os.path.exists(log_path):
+            os.makedirs(log_path)
+        # 日志文件名按照程序运行时间设置
+        log_file_name = log_path + 'log-' + time.strftime("%Y%m%d-%H%M%S", time.localtime()) + '.log'
+        # 记录正常的 print 信息
+        sys.stdout = Logger(log_file_name)
+        # 记录 traceback 异常信息
+        sys.stderr = Logger(log_file_name)
+
+        # 登录微信公众号，获取登录之后的cookies信息，并保存到本地文本中
+        weChat_login()
+        # 登录之后，通过微信公众号后台提供的微信公众号文章接口爬取文章
+        for query in gzlist:
+            # 爬取微信公众号文章，并存在本地文本中
+            print("开始爬取公众号：" + query)
+            get_content(query)
+            # time.sleep(60)
+            print("爬取完成")
+    except Exception as e:
+        print(str(e))
